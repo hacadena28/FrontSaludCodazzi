031ba631f5f328de8b201f2b2f5091b38a31631d
Victor Venegas <victorvenegas07@gmail.com>
2023-11-27 01:04:39 -0500

    Auth created

--- src/app/feature/feature-routing.module.ts
+++ src/app/feature/feature-routing.module.ts
@@ -4,6 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path:'', redirectTo:'home',pathMatch:'full' },
   { path: 'home', loadChildren:() => import('./home/home.module').then(m => m.HomeModule) },
+  { path: 'auth', loadChildren:() => import('./auth/auth.module').then(m => m.AuthModule) },
+
 ];
 
 @NgModule({
