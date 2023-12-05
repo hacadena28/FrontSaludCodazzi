332ea19fa13723af6ca22cecfa8389d048ee7ddd
Heli Cadena <heli.cadena@devzeros.com>
2023-12-04 21:28:49 -0500

    fixed layout patient and doctor

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,6 @@ export const environment = {
   appConfig: 'dev',
   pathConfig: './assets/config',
   apiUrl: '/renovables',
-  appUrl: 'https://clinica-codazi.azurewebsites.net/api/',
+  // appUrl: 'https://clinica-codazi.azurewebsites.net/api/',
+  appUrl: 'https://localhost:5001/api/',
 };
